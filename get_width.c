32400611512e13d53b89789d134f3f5986e8ace0
CodewithSavvy <123278508+CodewithSavvy@users.noreply.github.com>
2023-03-28 03:07:30 +0000

    update

--- get_width.c
+++ get_width.c
@@ -5,7 +5,7 @@
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
+ * Task done by Sekuo and Antwi
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
