a6b9759bc3c16c7aabe4ea80bdd963e5e57957eb
CodewithSavvy <123278508+CodewithSavvy@users.noreply.github.com>
2023-03-28 03:01:24 +0000

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -6,6 +6,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ * Task done by Sekuo and Antwi.
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
