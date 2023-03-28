9bb6aef12ca6af040298974ff2e579be14c73905
CodewithSavvy <123278508+CodewithSavvy@users.noreply.github.com>
2023-03-28 02:58:45 +0000

    Update _printf.c
    update

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * Task done by Sekuo and Antwi.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
